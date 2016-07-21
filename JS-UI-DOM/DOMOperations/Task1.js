6183bb33372bc5f547160c43e9b017806b5f0473
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2016-07-22 00:48:28 +0300

    up

---
+++ JS-UI-DOM/DOMOperations/Task1.js
@@ -0,0 +1,8 @@
+function solve() {
+  return function(element, contents) {
+      for (var i = 0, len=contents.children.length; i < array.length; i++) {
+          var elem, len=contents.children.lengthent = array[i];
+          
+      }
+  };
+}
