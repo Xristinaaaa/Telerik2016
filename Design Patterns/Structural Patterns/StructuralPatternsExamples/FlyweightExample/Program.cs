489df1618e75eb8250ec3b88ba724d9f49a770b9
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2016-10-28 19:52:49 +0300

    update design patterns

--- Design Patterns/Structural Patterns/StructuralPatternsExamples/FlyweightExample/Program.cs
+++ Design Patterns/Structural Patterns/StructuralPatternsExamples/FlyweightExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FlyweightExample.Contracts;
+using FlyweightExample.Enums;
+using FlyweightExample.Models;
+using System;
 
 namespace FlyweightExample
 {
@@ -6,6 +9,30 @@ namespace FlyweightExample
     {
         static void Main(string[] args)
         {
+            const int ONE_MILLION = 10000;
+            int[] currencyDenominations = new[] { 1, 5, 10, 20, 50, 100 };
+            MoneyFactory moneyFactory = new MoneyFactory();
+            int sum = 0;
+
+            while (sum <= ONE_MILLION)
+            {
+                IMoney graphicalMoneyObj = null;
+                Random rand = new Random();
+                int currencyDisplayValue = currencyDenominations[rand.Next(0, currencyDenominations.Length)];
+
+                if (currencyDisplayValue == 1 || currencyDisplayValue == 5)
+                {
+                    graphicalMoneyObj = moneyFactory.GetMoneyToDisplay(EnMoneyType.Metallic);
+                }
+                else
+                {
+                    graphicalMoneyObj = moneyFactory.GetMoneyToDisplay(EnMoneyType.Paper);
+                }
+
+                graphicalMoneyObj.GetDisplayOfMoneyFalling(currencyDisplayValue);
+                sum = sum + currencyDisplayValue;
+            }
+            Console.WriteLine("Total Objects created=" + MoneyFactory.ObjectsCount.ToString());
         }
     }
 }
